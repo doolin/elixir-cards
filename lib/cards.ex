fe36fe6f9e67d1d7403f75b09a15dd666b3e8d75
dave doolin <david.doolin>
2018-11-18 07:24:37 -0800

    video 13, 14 flatten list for deck

--- lib/cards.ex
+++ lib/cards.ex
@@ -20,9 +20,12 @@ defmodule Cards do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-    for suit <- suits do
-      suit
+    cards = for value <- values do
+      for suit <- suits do
+        "#{value} of #{suit}"
+      end
     end
+    List.flatten(cards)
   end
 
   def shuffle(deck) do
